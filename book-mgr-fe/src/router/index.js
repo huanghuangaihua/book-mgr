0fd12fb79b8adac1772807c3f997482d591d151c
Your Name <you@example.com>
2021-05-04 21:26:44 +0800

    完成基础布局

--- book-mgr-fe/src/router/index.js
+++ book-mgr-fe/src/router/index.js
@@ -5,13 +5,21 @@ const routes = [
   {
     path: '/auth',
     name: 'Auth',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Auth/index.vue')
+    component: () => import(/* webpackChunkName: "auth" */ '../views/Auth/index.vue')
   },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+  {
+    path: '/',
+    name: 'BasicLayout',
+    component: () => import(/* webpackChunkName: "BasicLayout" */ '../layout/BasicLayout/index.vue'),
+    children: [/* 子路径*/
+      {
+        path: 'books',
+        name: 'Books',
+        component: () => import(/* webpackChunkName: "Book" */ '../views/Books/index.vue'),
+      },
+      
+    ],
+  },
 ]
 
 const router = createRouter({
